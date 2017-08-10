a0c4853bcf9eb8594d857cbadac1a053bcfe5702
rhuberdeau <rhuberdeau@gmail.com>
2017-08-10 13:25:57 -0400

    Removing Minitest tests

--- test/dummy/config/initializers/templarbit.rb
+++
@@ -1,8 +0,0 @@
-Rails.configuration.templarbit = {
-  :api_key => ENV['TB_API_KEY'],
-  :property_id => ENV['TB_PROPERTY_ID']
-}
-
-Templarbit.api_key = Rails.configuration.templarbit[:api_key]
-Templarbit.property_id = Rails.configuration.templarbit[:property_id]
-
